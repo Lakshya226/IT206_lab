d2787ef43a6847ea2562432a979cab6348063be9
Lakshya226 <202401220@daiict.ac.in>
2025-04-04 14:07:28 +0530

    Update two-sum.cpp

--- two-sum.cpp
+++ two-sum.cpp
@@ -5,9 +5,11 @@ public:
         vector<int> index;
         unordered_map<int,int> ind;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             int c=target-nums[i];
-            if (ind.find(c)!=ind.end() && ind[c] != i) {
+            if (ind.find(c)!=ind.end() && ind[c] != i) 
+            {
                 index.push_back(ind[c]);
                 index.push_back(i);
                 return ans;
